4996eda758713267c910982dea03374a9f630198
Lizeth Martínez <lizethm745@gmail.com>
2023-11-02 16:32:36 -0500

    campo txt muestra y guarda

--- src/wall.js
+++ src/wall.js
@@ -8,26 +8,31 @@ function wall() {
   const divContenedor = document.createElement('div');
   divContenedor.setAttribute('class', 'contenedorWall');
 
-  const txtCrearpost = document.createElement('textarea');
-  txtCrearpost.placeholder = 'escribe aqui tu post';
+  const crearPost = document.createElement('textarea');
+  crearPost.placeholder = 'escribe aqui tu post';
   const btnpost = document.createElement('button');
   // Configura el texto del botón
   btnpost.innerText = 'Haz clic en mí';
   const postCreado = document.createElement('textarea');
   /* postCreado.textContent = ''; */
 
-  divContenedor.append(txtCrearpost, btnpost, postCreado);
+  divContenedor.append(crearPost, btnpost, postCreado);
   section.appendChild(divContenedor);
 
   btnpost.addEventListener('click', () => {
     const newPost = {
       date: new Date(),
-      text: postCreado.value,
+      text: crearPost.value, // guarda lo que el usuario escribio
     };
 
     createPost(newPost)
       .then((resultado) => {
         console.log(resultado, 'post creado');
+        // Actualiza el contenido de postCreado con el contenido de crearpost
+        postCreado.textContent = crearPost.value;
+
+        // Limpia el campo crearpost
+        crearPost.value = '';
       })
       .catch((err) => {
         console.log(err, 'error al crear post');
