a0fbd471d4787535ac630d98902bc2d108be8e61
Lizeth Martínez <lizethm745@gmail.com>
2023-11-09 10:46:30 -0500

    probando actualizacion del ultimo commit

--- src/firestore.js
+++ src/firestore.js
@@ -3,7 +3,7 @@ import {
 } from 'firebase/firestore';
 import { app } from './firebase';
 
-const db = getFirestore(app); // Obtenga una referencia a la base de datos de Firestore:
+const db = getFirestore(app); // Obtenga una referencia a la base de datoss de Firestore:
 // crear una nueva publicación en la base de datos Firestore.
 export const createPost = (obj) => addDoc(collection(db, 'post'), {
   ...obj,
