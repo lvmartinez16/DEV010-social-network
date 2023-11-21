8c29806cc05d5b6627e28417e6ee98a178e1b42e
Lizeth Martínez <lizethm745@gmail.com>
2023-11-20 19:11:19 -0500

    estilo de post

--- src/wall.js
+++ src/wall.js
@@ -2,36 +2,28 @@
 /* eslint-disable no-unused-vars */
 import { createPost, obtenerPosts } from './firestore';
 
-function wall() {
+function wall(onNavigate) {
   const sectionW = document.createElement('section');
   sectionW.className = 'sectionW';
   const divContenedor = document.createElement('div');
   divContenedor.setAttribute('class', 'contenedorWall');
 
-  const crearPost = document.createElement('textarea');
+  const btnCerrar = document.createElement('button');
+  btnCerrar.id = 'btnSalir';
+  btnCerrar.textContent = 'salir';
+
+  const crearPost = document.createElement('input');
+  crearPost.id = 'crearPost';
   crearPost.placeholder = 'Escribe aquí tu post';
   const btnPost = document.createElement('button');
   btnPost.innerText = 'Enviar';
-  btnPost.className = 'btnPost';
+  btnPost.id = 'btnPost';
   const postCreado = document.createElement('div');
-  postCreado.className = 'sesionPost';
+  postCreado.id = 'sesionPost';
   // postCreado.append(obtenerPosts());
   divContenedor.append(crearPost, btnPost, postCreado);
   sectionW.appendChild(divContenedor);
 
-  // function mostrarPosts(posts) {
-  //   postCreado.innerHTML = ''; // Clear existing posts
-
-  //   // Reverse the order of posts to display the newest ones first
-  //   posts.reverse();
-
-  //   posts.forEach((post) => {
-  //     const postElement = document.createElement('div');
-  //     postElement.className = 'post';
-  //     postElement.textContent = `${post.text} - ${post.date}`;
-  //     postCreado.appendChild(postElement);
-  //   });
-  // }
   // Event listener for the "Enviar" button
   btnPost.addEventListener('click', () => {
     const newPost = {
@@ -50,7 +42,6 @@ function wall() {
         console.error(err, 'Error al crear post');
       });
   });
-
   // Initial rendering of posts when the page loads
   // obtenerPosts((posts) => {
   //   mostrarPosts(posts);
